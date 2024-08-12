af5b4681bdd03aa1a8d1887fdc3e7ff32615dd80
jie_w <jie_wu@asus.com>
2024-08-12 10:57:21 +0800

    Optimize

--- linux/env_remove.sh
+++ linux/env_remove.sh
@@ -10,12 +10,12 @@ fi
 # 指定要删除的 conda 环境名称
 ENV_NAME=FlexTuner
 
-# 确认用户输入
-read -p "你确定要卸载环境 $ENV_NAME 吗？(y/n): " CONFIRM
-if [ "$CONFIRM" != "y" ]; then
-    echo "操作已取消。"
-    exit 1
-fi
+# # 确认用户输入
+# read -p "你确定要卸载环境 $ENV_NAME 吗？(y/n): " CONFIRM
+# if [ "$CONFIRM" != "y" ]; then
+#     echo "操作已取消。"
+#     exit 1
+# fi
 
 # 卸载选定的环境
 conda env remove -n "$ENV_NAME"
--- windows/install_wsl.bat
+++ windows/install_wsl.bat
@@ -28,14 +28,8 @@ wsl --update
 :: 安装 Ubuntu 发行版
 echo Installing Ubuntu distribution. This may take a while...
 :: 提示用户首次启动 Ubuntu 控制台时，需要手动输入 exit 并按回车退出，以确保更新正常
-echo Please exit the Ubuntu console by typing 'exit' to complete the installation.
+:: echo Please exit the Ubuntu console by typing 'exit' to complete the installation.
 wsl --install -d Ubuntu
-
-:: 写入标记到文件，表示用户上一步输入exit退出,而不是直接关闭窗口
-:: 输入退出可以继续下面步骤，直接关闭窗口会导致下面步骤无法执行
-
-:: 将分布版设置为默认值
-wsl --set-default Ubuntu
 
 echo WSL2 installation and configuration complete.
 pause
