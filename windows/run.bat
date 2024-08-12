ddc7d532d25307e22daa58b0a38ae1fbed401b4e
jie_w <jie_wu@asus.com>
2024-08-12 15:01:00 +0800

    fix HOME path

--- windows/run.bat
+++ windows/run.bat
@@ -6,6 +6,6 @@ for /f "tokens=* USEBACKQ" %%F in (`wsl whoami`) do set "wsl_username=%%F"
 
 :: 在 WSL conda ENV_NAME 环境中执行脚本
 :: 启动lmf-cli webui
-wsl -e bash -ic "source ~/.bashrc && conda activate %ENV_NAME% && conda env list && cd /home/%wsl_username%/%ENV_NAME%/src && pwd && llamafactory-cli webui"
+wsl -e bash -ic "source ~/.bashrc && conda activate %ENV_NAME% && conda env list && cd $HOME/%ENV_NAME%/src && pwd && llamafactory-cli webui"
 
 pause
