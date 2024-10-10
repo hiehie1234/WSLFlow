773045fccd0464d9c2a9e3858e64c805573ce404
jie_w <jie_wu@asus.com>
2024-10-10 16:43:24 +0800

    fix 补充缺失文件

---
+++ windows/cal_mem.bat
@@ -0,0 +1,9 @@
+@echo off
+set "ENV_NAME=FlexTuner"
+
+wsl -d ASUS-Workbench -e bash -ic "source ~/.bashrc && conda activate %ENV_NAME% && conda env list && cd /usr/share/asus-llm/env && pwd && ./cal_mem.sh %1"
+if %errorlevel% neq 0 (
+    echo llamafactory-cli error
+    exit /b %errorlevel%
+)
+exit 0
